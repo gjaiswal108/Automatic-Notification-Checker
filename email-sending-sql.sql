de8e271a8acae423239df62ba2e542a22a2e8a7a
Gaurav Jaiswal <32886368+gjaiswal108@users.noreply.github.com>
2019-06-17 01:21:03 +0530

    Add files via upload

---
+++ email-sending-sql.sql
@@ -0,0 +1,8 @@
+create database user;
+use user;
+create table data(notification varchar(1000));
+insert into data values('hi');
+create table notice(notification varchar(1000));
+insert into notice values('hi');
+create table emailList(email varchar(100));
+insert into emailList values('gjaiswal108@gmail.com');
